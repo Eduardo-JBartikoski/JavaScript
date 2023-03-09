34b12cb1b4ef63e4f98d0d9acd75e224c9ce091b
FamosoReverse <eduardob.i207@gmail.com>
2023-03-09 14:26:56 -0300

    att

--- aulas/aula12/ex12.js
+++ aulas/aula12/ex12.js
@@ -1,9 +1,13 @@
 var idade = 16
+console.log(`você tem a ${idade} anos.`)
 if (idade < 16) {
     console.log('não vota')
 } else {
-    if (idade >= 16 && idade < 18){
+    if (idade < 18 || idade > 65){
         console.log('voto opcional')
+    } else { 
+        console.log('Voto obrigatório')
     }
+    
 }
 
