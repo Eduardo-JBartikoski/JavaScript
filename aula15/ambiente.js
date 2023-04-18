8eb09a576311e88ed60b9aaa425728b29e1cad72
FamosoReverse <eduardob.i207@gmail.com>
2023-04-18 14:37:12 -0300

    att

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,19 @@
-let num = [5,8,2]
-console.log(`nosso vetor é o ${num}`)
-num [3]= 3
+{let num = [9,2,3,6,5,7]
+num.sort()
+let pos = num.length
+for(let pos in num){
+    console.log(`Essa é a posição ${pos} que tem o valor de  ${num[pos]}`)
+}
+}
+
+let num = [9,6,3,5,2]
+num.sort()
+let pos = num.indexOf(3)
+if(pos == -1){
+    console.log('Esse número não existe')
+} else {
+console.log(`o valor está na posição ${pos}`)}
+
+
+
+
